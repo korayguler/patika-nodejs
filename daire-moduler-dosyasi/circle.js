58ea8978b191e162e48b5ae4f5d5c7c7f3903c42
Koray Güler <us.koray.guler@gmail.com>
2021-05-23 01:19:28 +0300

    daire moduler dosyasi add

---
+++ daire-moduler-dosyasi/circle.js
@@ -0,0 +1,9 @@
+const circleArea = (r) => {
+  console.log(Math.PI * r ** 2);
+};
+
+const circleCircumference = (r) => {
+  console.log(2 * Math.PI * r);
+};
+
+module.exports = { circleArea, circleCircumference };
